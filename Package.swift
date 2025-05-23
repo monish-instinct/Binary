2d2a61fc40e4d4fda9c3a00ebcbf66454a00079e
InstincT <113701884+monish-instinct@users.noreply.github.com>
2025-05-23 20:21:10 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "MyNeuralFramework",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "MyNeuralFramework",
+            targets: ["MyNeuralFrameworkWrapper"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/stoneburner/sqlcipher.git",
+            from: "4.5.0"
+        ),
+        .package(
+            url: "https://github.com/razorpay/razorpay-pod.git",
+            from: "1.0.0"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyNeuralFramework",
+            url: "https://github.com/monish-instinct/Binary/releases/download/binary/MyNeuralFramework.xcframework.zip",
+            checksum: "e6fd122c3d086f262f677732fd0eb61614ae06723483bdd7d5046fee8248fbf0"
+        ),
+        .target(
+            name: "MyNeuralFrameworkWrapper",
+            dependencies: [
+                "MyNeuralFramework",
+                .product(name: "SQLCipher", package: "sqlcipher"),
+                .product(name: "razorpay-pod", package: "razorpay-pod")
+            ],
+            path: "Sources/MyNeuralFrameworkWrapper"
+        )
+    ]
+)
